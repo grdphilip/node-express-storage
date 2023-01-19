84fe0704a2c8aa693b2667fa1986d2e22d40ff3a
grdphilip <philiprettig11@gmail.com>
2023-01-19 14:00:49 +0100

    Put route added

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -4,7 +4,6 @@ const Article = require("../models/articleModel");
 
 // define the home page route
 router.get("/", async (req, res) => {
-
   try {
     const articles = await Article.find();
     res.json(articles);
@@ -15,7 +14,6 @@ router.get("/", async (req, res) => {
 
 // define the about route
 router.post("/", async (req, res) => {
-
   const article = new Article({
     name: req.body.name,
     compartments: req.body.compartments,
@@ -24,7 +22,7 @@ router.post("/", async (req, res) => {
     supplier: req.body.supplier,
   });
 
-  console.log(article)
+  console.log(article);
 
   try {
     const newArticle = await article.save();
@@ -34,12 +32,23 @@ router.post("/", async (req, res) => {
   }
 });
 
-
 router.put("/:id", async (req, res) => {
-  
-  
   try {
-    const article = await Article.updateOne(req.params.id)
+    const article = await Article.findById(req.params.id);
+
+    if (!article) {
+      return res.status(404).json({ message: "Article not found" });
+    }
+    
+    article.name = req.body.name;
+    article.compartments = req.body.compartments;
+    article.lioNr = req.body.lioNr;
+    article.price = req.body.price;
+    article.supplier = req.body.supplier;
+
+    await article.save();
+
+    res.json(article);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
